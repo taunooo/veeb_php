ce658df52ec121dd70d6e388eee15a01c8ed0111
tauno.peepson <tauno.peepson@khk.ee>
2019-02-13 14:21:58 +0200

    Muutujate loomine
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

---
+++ php_alused/Variables/index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tauno.peepson
+ * Date: 13.02.2019
+ * Time: 13:51
+ */
+
+//Ülesanne 1 Tauno Peepson 13.02
+$enimi = "Tauno";
+$pnimi = "Peepson";
+$vanus = 19;
+$sugu = "M";
+
+$nimi = $enimi.' '.$pnimi;
+echo "$nimi $vanus $sugu";
+echo '<br>';
+//var_dump($nimi); Muutjua sisu. (string(13) "Tauno Peepson")
+?>
+
+
+
+
