0ea29bd7041dbf5c38e03c182d61cce1e9bbc295
tauno.peepson <tauno.peepson@khk.ee>
2019-02-13 12:40:23 +0200

    README faili uuendus
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

---
+++ php_alused/print/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tauno.peepson
+ * Date: 13.02.2019
+ * Time: 12:14
+ */
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
+
+echo '<hr>';
+
+echo "Hello World!";
+echo "Hello World!";
+echo "Hello World!";
