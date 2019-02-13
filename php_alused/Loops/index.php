3f5001e9763d1ff7da86056d6be8290f2b42ad5d
tauno.peepson <tauno.peepson@khk.ee>
2019-02-13 15:31:50 +0200

    Loop, Refresh iga 5 sekundi tagant
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

---
+++ php_alused/Loops/index.php
@@ -0,0 +1,36 @@
+<?php
+for($kord = 1; $kord <= 10; $kord++){
+header('refresh:5');
+}
+?>
+<style>
+    div {
+        margin: auto;
+        width: 100px;
+        height: 100px;
+        font-size: 70px;
+        padding-top: 15px;
+        text-align: center;
+    }
+    .paaris{
+        background: red;
+    }
+    .paaritu{
+        background: green;
+    }
+</style>
+<?php
+
+// genereerime juhuslik täisarv vahemikus 0 kuni 100
+$arv = rand(0, 100);
+// arvutame jääk 2-ga jagamisel
+$jaak = $arv % 2;
+// kontrollime kui jääk on 0 - paaris arv
+if($jaak == 0){
+    echo '<div class="paaris">'.$arv.'</div>';
+} else {
+    echo '<div class="paaritu">'.$arv.'</div>';
+}
+
+
+
