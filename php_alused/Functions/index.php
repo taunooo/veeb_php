b7139795514e870f24fbfc2a0c81c2132634ccba
tauno.peepson <tauno.peepson@khk.ee>
2019-02-20 14:47:42 +0200

    Funkitsioonid eraldi failis
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

--- php_alused/Functions/index.php
+++ php_alused/Functions/index.php
@@ -1,19 +1,5 @@
 <?php
-
-
-function paarsuseKontroll ($arv){
-     if ($arv % 2 == 0) {
-         $kontroll = $arv . ' on paaris! <br>';
-     } else {
-         $kontroll = $arv.' on paaritu!<br>';
-         }
-         return $kontroll;
-}
-
-// Kutsume esile funktsiooni
-
-for ($arv = 0; $arv <= 10; $arv++) {
-   $kontroll = paarsuseKontroll($arv);
-    echo $kontroll;
-}
-
+require_once 'fnc.php';
+lehePais();
+tabel(15);
+?>
