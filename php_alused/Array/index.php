d7c130c01084beae5dfdfed62d0929d6bf5a1687
tauno.peepson <tauno.peepson@khk.ee>
2019-02-20 15:31:29 +0200

    Peppa pig ja George array
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

--- php_alused/Array/index.php
+++ php_alused/Array/index.php
@@ -1,24 +1,44 @@
 <?php
+
+$porsad = array
+(
     $pepaPig = array(
         'Peppa',
         'naine',
         4,
         1.04
-    );
+    ),
+    $georgePig = array(
+        'George',
+        'mees',
+        2,
+        0.5
+    ),
+
+);
+
+
 
 /*    var_dump($pepaPig);
     echo '<pre>';
     print_r($pepaPig);
     echo '</pre>';*/
 
-echo $pepaPig[0].'<br>';
-echo $pepaPig[1].'<br>';
-echo $pepaPig[2].'<br>';
-echo $pepaPig[3].'<br>';
+echo $porsad[0].'<br>';
+echo $porsad[1].'<br>';
+echo $porsad[2].'<br>';
+echo $porsad[3].'<br>';
 
 echo '<hr>';
 
 for($i = 0;$i < count($pepaPig); $i++)
     echo $pepaPig[$i].'<br>';
+
+echo '<hr>';
+
+foreach ($porsad as $porsas) {
+    foreach ($porsas as $element)
+        echo $element.'<br>';}
+
 
 
