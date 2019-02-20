cd366266d30a92a9d7721ae9f03e3c0b4b289d52
tauno.peepson <tauno.peepson@khk.ee>
2019-02-20 15:14:25 +0200

    Array, loodud lihtne massiiv PeppaPig
    Signed-off-by: tauno.peepson <tauno.peepson@khk.ee>

--- php_alused/Array/index.php
+++ php_alused/Array/index.php
@@ -5,7 +5,20 @@
         4,
         1.04
     );
-    var_dump($pepaPig);
+
+/*    var_dump($pepaPig);
     echo '<pre>';
     print_r($pepaPig);
-    echo '</pre>';
+    echo '</pre>';*/
+
+echo $pepaPig[0].'<br>';
+echo $pepaPig[1].'<br>';
+echo $pepaPig[2].'<br>';
+echo $pepaPig[3].'<br>';
+
+echo '<hr>';
+
+for($i = 0;$i < count($pepaPig); $i++)
+    echo $pepaPig[$i].'<br>';
+
+
