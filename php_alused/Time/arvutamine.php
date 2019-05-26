702752ab5780137b05f191a79b602fec2e9459c2
Taunooo <tauno.peepson@khk.ee>
2019-05-26 22:33:14 +0300

    Aeg - ajatöötlus

---
+++ php_alused/Time/arvutamine.php
@@ -0,0 +1,5 @@
+<?php
+
+echo date('d.m.Y G:i' , time()+60);
+echo date('d.m.Y G:i' , time()+60*60);
+echo date('d.m.Y G:i' , time()+60*60*24);
---
+++ php_alused/Time/kuupaev.php
@@ -0,0 +1,10 @@
+<?php
+$eesti_kuud = array(1=>'jaanuar', 'veebruar', 'märts', 'aprill', 'mai', 'juuni', 'juuli', 'august', 'september', 'oktoober', 'november', 'detsember');
+//kuupäevad massiividesse
+
+$paev = date('d');
+$kuu = $eesti_kuud[date('n')];
+$aasta = date('Y');
+
+//kuupäeva väljastamine
+echo $paev.'.'.$kuu.' '.$aasta;
